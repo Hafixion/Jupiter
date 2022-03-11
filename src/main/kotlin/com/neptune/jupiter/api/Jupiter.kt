b3fa1cbb4910b7d013cd1df5161336b612761102
Hafixion <70385526+Hafixion@users.noreply.github.com>
2022-03-11 10:38:21 +0200

    fixing some stuff

--- src/main/kotlin/com/neptune/jupiter/api/Jupiter.kt
+++ src/main/kotlin/com/neptune/jupiter/api/Jupiter.kt
@@ -1,6 +1,5 @@
 package com.neptune.jupiter.api
 
-
 import com.neptune.jupiter.api.controllers.*
 import com.neptune.jupiter.api.controllers.BungeeCordController
 import com.neptune.jupiter.api.controllers.CommandController
@@ -8,14 +7,13 @@ import com.neptune.jupiter.api.controllers.MenuController
 import com.neptune.jupiter.api.controllers.PlayerController
 import com.neptune.jupiter.api.controllers.ProviderController
 import com.neptune.jupiter.api.extensions.plugin.registerEvents
-import org.bstats.bukkit.Metrics
 import org.bukkit.Bukkit
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
 internal fun provideKotlinBukkitAPI(): Jupiter {
-    return Bukkit.getServer().pluginManager.getPlugin("KotlinBukkitAPI") as Jupiter?
-            ?: throw IllegalAccessException("The plugin KotlinBukkitAPI is not loaded yet")
+    return Bukkit.getServer().pluginManager.getPlugin("JupiterApi") as Jupiter?
+            ?: throw IllegalAccessException("The plugin JupiterApi is not loaded yet")
 }
 
 private const val BSTATS_PLUGIN_ID = 6356
@@ -27,8 +25,6 @@ class Jupiter : JavaPlugin() {
     internal val playerController = PlayerController(this)
     internal val providerController = ProviderController(this)
     internal val bungeeCordController = BungeeCordController(this)
-
-    internal lateinit var metrics: Metrics private set
 
     private val controllers = listOf(
             commandController, menuController, playerController,
@@ -42,8 +38,5 @@ class Jupiter : JavaPlugin() {
             if(controller is Listener)
                 registerEvents(controller)
         }
-
-        // setup metrics
-        metrics = Metrics(this, BSTATS_PLUGIN_ID)
     }
 }
